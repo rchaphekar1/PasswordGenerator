0ea707e3b5081b6f702024dd5a7f4efb92f1491b
Rahul Chaphekar <rchaphekar1@gmail.com>
2020-12-23 21:54:40 -0600

    Added comments to script.js

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Initializes and sets the lower case, upper case, numeric, and special character arrays
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericCharacterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialCharacterArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+// Records array in console
 console.log(lowerCaseArray);
 console.log(upperCaseArray);
 console.log(numericCharacterArray);
 console.log(specialCharacterArray);
 
+// Defines the function to generate the random password
 function generatePassword() {
+  // Prompts the user to enter the number of characters, and saves it as variable passwordLength
   var passwordLength = prompt("How many characters should your password be?");
   console.log(passwordLength);
+  // Converts the user entered string to an integer
   passwordLength = parseInt(passwordLength, 10);
   console.log(passwordLength);
+  // Checks if the desired password length is too short or too long
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Passwords must be at least 8 characters and at most 128 characters.");
+    // Reprompts user if this is the case
     var passwordLength = prompt("How many characters should your password be?");
     console.log(passwordLength);
     passwordLength = parseInt(passwordLength, 10);
     console.log(passwordLength);
   }
+  // A series of confirm windows asking the user what types of characters should be included
   var lowerCase = confirm("Should lower case letters be included?");
   var upperCase = confirm("Should upper case letters be included?");
   var numericCharacter = confirm("Should numeric characters be included?");
@@ -29,8 +38,10 @@ function generatePassword() {
   console.log(upperCase);
   console.log(numericCharacter);
   console.log(specialCharacter);
+  // Checks to see if no character types were selected, and alerts the user if so
   if ((lowerCase !== true) && (upperCase !== true) && (numericCharacter !== true) && (specialCharacter !== true)) {
     alert("Please select at least one character type.");
+    // Reprompts the user if this is the case
     var lowerCase = confirm("Should lower case letters be included?");
     var upperCase = confirm("Should upper case letters be included?");
     var numericCharacter = confirm("Should numeric characters be included?");
@@ -41,7 +52,9 @@ function generatePassword() {
     console.log(specialCharacter);
   }
   
+  // Initializes availableCharacterArray as an empty array
   var availableCharacterArray = [];  
+  // Checks to see if each character type should be included, and concatenates them to the availableCharacterArray if so
   if (lowerCase === true) {
     availableCharacterArray = availableCharacterArray.concat(lowerCaseArray);
     console.log(availableCharacterArray);
@@ -59,18 +72,24 @@ function generatePassword() {
     console.log(availableCharacterArray);
   }
  
+  //Initializes the password as an empty string
   var i = 0;
   var passwordResult = "";
+  // Runs while loop while i is less than the password length
   while (i < passwordLength) {
+    // Adds characters at a randomly chosen index to the passwordResult string
     passwordResult += availableCharacterArray[Math.floor(Math.random() * availableCharacterArray.length)];
+    // Increments the counter i
     i++;
   }
   console.log(passwordResult);
+  // Returns the password after running the function
   return passwordResult;
 }
 
 // Write password to the #password input
 function writePassword() {
+  // Stores the passwordResult variable returned by the generatePassword function and stores it in new variable password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
